cebfb20068a6f1f6919a2b49f7123b4e9de9c90d
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2022-09-07 10:04:24 +0200

    preparing hamburguer menu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use sctrict"
+
+document.querySelector(".hamburguer").addEventListener("click", function(){  
+    document.querySelector(".full-menu").classList.toggle("active");  
+    document.querySelector(".hamburguer").classList.toggle("close-hamburguer");  
+});
